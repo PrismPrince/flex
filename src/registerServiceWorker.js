a74113e978cd2a29c3888d78854158834a889be6
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2018-07-05 00:07:25 +0800

    Add click event [bug]

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated (registration) {
       console.log('New content is available; please refresh.')
-      messaging.useServiceWorker(registration)
+      // messaging.useServiceWorker(registration)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
